c7908373f2346901b3aac94f905656fc382d1e2a
Wensheng Liu <Wensheng.Liu@healthshare.co.nz>
2022-02-25 13:12:14 +1300

    Set up database for contacts

---
+++ src/Database/Tables/Contacts.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[Contacts]
+(
+	[Id] BIGINT NOT NULL IDENTITY, 
+    [ContactUsageId] BIGINT NOT NULL, 
+    [ContactTypeId] BIGINT NOT NULL, 
+    [Detail] NVARCHAR(255) NOT NULL, 
+    [IsProtected] BIT NOT NULL DEFAULT 0, 
+    [EffectiveFrom] NVARCHAR(8) NOT NULL, 
+    [EffectiveTo] NVARCHAR(8) NULL, 
+    [IsPreferred] BIT NOT NULL DEFAULT 0
+    CONSTRAINT [PK_Contacts] PRIMARY KEY ([Id]),
+	CONSTRAINT [FK_Contacts_ContactUsages_ContactUsageId] FOREIGN KEY ([ContactUsageId]) REFERENCES [ContactUsages]([Id]),
+    CONSTRAINT [FK_Contacts_ContactTypes_ContactTypeId] FOREIGN KEY ([ContactTypeId]) REFERENCES [ContactTypes]([Id])
+)
