1c55cd08344edd49bd094783759c482dd704d321
Nripendra.Pradhananga <Nripendra.Pradhananga@healthshare.co.nz>
2022-03-07 11:34:33 +1300

    updating sql script and testing

--- sql/seed-script.sql
+++ sql/seed-script.sql
@@ -4,25 +4,28 @@
 BEGIN TRANSACTION;
 GO
 
-IF EXISTS (SELECT TOP 1 1 FROM [AddressFormats])
+IF NOT EXISTS (SELECT TOP 1 1 FROM [AddressFormats])
 BEGIN 
-IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [name] IN (N'Id', N'Code', N'Comment', N'Description') AND [object_id] = OBJECT_ID(N'[AddressFormats]'))
-    SET IDENTITY_INSERT [AddressFormats] ON;
-INSERT INTO [AddressFormats] ([Id], [Code], [Comment], [Description])
-VALUES (CAST(1 AS bigint), N'CIQ', N'', N'NZ CIQ Address Profile'),
-(CAST(2 AS bigint), N'POST', N'', N'NZ Post Address Standard');
-IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [name] IN (N'Id', N'Code', N'Comment', N'Description') AND [object_id] = OBJECT_ID(N'[AddressFormats]'))
-    SET IDENTITY_INSERT [AddressFormats] OFF;
+    IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [name] IN (N'Id', N'Code', N'Comment', N'Description') AND [object_id] = OBJECT_ID(N'[AddressFormats]'))
+        SET IDENTITY_INSERT [AddressFormats] ON;
+    INSERT INTO [AddressFormats] ([Id], [Code], [Comment], [Description])
+    VALUES (CAST(1 AS bigint), N'CIQ', N'', N'NZ CIQ Address Profile'),
+    (CAST(2 AS bigint), N'POST', N'', N'NZ Post Address Standard');
+    IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [name] IN (N'Id', N'Code', N'Comment', N'Description') AND [object_id] = OBJECT_ID(N'[AddressFormats]'))
+        SET IDENTITY_INSERT [AddressFormats] OFF;
 END
 GO
 
-IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [name] IN (N'Id', N'Code', N'Comment', N'Description', N'HL7PostalAddressUse') AND [object_id] = OBJECT_ID(N'[AddressTypes]'))
-    SET IDENTITY_INSERT [AddressTypes] ON;
-INSERT INTO [AddressTypes] ([Id], [Code], [Comment], [Description], [HL7PostalAddressUse])
-VALUES (CAST(1 AS bigint), N'M', N'', N'Mailing', N'PST'),
-(CAST(2 AS bigint), N'R', N'', N'Residential', N'H');
-IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [name] IN (N'Id', N'Code', N'Comment', N'Description', N'HL7PostalAddressUse') AND [object_id] = OBJECT_ID(N'[AddressTypes]'))
-    SET IDENTITY_INSERT [AddressTypes] OFF;
+IF NOT EXISTS (SELECT TOP 1 1 FROM [AddressTypes])
+BEGIN 
+    IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [name] IN (N'Id', N'Code', N'Comment', N'Description', N'HL7PostalAddressUse') AND [object_id] = OBJECT_ID(N'[AddressTypes]'))
+        SET IDENTITY_INSERT [AddressTypes] ON;
+    INSERT INTO [AddressTypes] ([Id], [Code], [Comment], [Description], [HL7PostalAddressUse])
+    VALUES (CAST(1 AS bigint), N'M', N'', N'Mailing', N'PST'),
+    (CAST(2 AS bigint), N'R', N'', N'Residential', N'H');
+    IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [name] IN (N'Id', N'Code', N'Comment', N'Description', N'HL7PostalAddressUse') AND [object_id] = OBJECT_ID(N'[AddressTypes]'))
+        SET IDENTITY_INSERT [AddressTypes] OFF;
+END
 GO
 
 IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [name] IN (N'Id', N'Code', N'Comment', N'Description') AND [object_id] = OBJECT_ID(N'[BirthDateSources]'))
