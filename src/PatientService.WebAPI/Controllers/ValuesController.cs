6802be6ccd742a4a9c9f8efa312add0a8920dda7
Nripendra.Pradhananga <Nripendra.Pradhananga@healthshare.co.nz>
2022-03-10 14:45:01 +1300

    testing pipeline

--- src/PatientService.WebAPI/Controllers/ValuesController.cs
+++ src/PatientService.WebAPI/Controllers/ValuesController.cs
@@ -13,7 +13,7 @@ namespace PatientService.WebAPI.Controllers
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2", "value11", "value12", "value13" };
+            return new string[] { "value1", "value2", "value11", "value12" };
         }
 
         // GET api/values/5
