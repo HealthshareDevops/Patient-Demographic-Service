9e473a57577ef5a90a284af0d9328f91cf0ddc70
Somitha Joseph <Somitha.Joseph@healthshare.co.nz>
2022-06-14 11:24:34 +1200

    MHIP-147-NSP-99

--- src/Application/Commands/CreatePatient/CreatePatientCommand.cs
+++ src/Application/Commands/CreatePatient/CreatePatientCommand.cs
@@ -135,6 +135,9 @@ namespace Application.Commands.CreatePatient
                     // Add Ethnicities
                     foreach (var ethnicityCommand in request.Ethnicities)
                     {
+                        //MHIP-147 - Tactial solution - MHIP-148 would resolve this with a permanent code set solution.
+                        if (ethnicityCommand.Code == "NSP")
+                            ethnicityCommand.Code = "99";
                         var ethnicity = Ethnicity.FromCode(ethnicityCommand.Code);
                         if (ethnicity is null)
                         {
